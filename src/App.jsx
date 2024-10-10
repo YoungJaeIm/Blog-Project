1ad77fb4751eaf47ceaa7bcfab3110ebf9c9debb
YoungJaeIm <wpem2014@naver.com>
2024-10-10 18:38:21 +0900

    week 2

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,15 @@ import './App.css'
 function App() {
   const [posts, setPosts] = useState([])
 
+  //게시글 추가
   const addPost = (newPost) => {
     setPosts([...posts, newPost])
   }
+
+  // 게시글 삭제
+  const deletePost = (indexToDelete) => {
+    setPosts(posts.filter((_, index) => index !== indexToDelete));
+  };
 
   return (
     <div className="App">
@@ -17,7 +23,9 @@ function App() {
       <PostInput onAddPost={addPost} />
       <ul className="post-list">
         {posts.map((post, index) => (
-          <li key={index} className="post-item">{post}</li>
+          <li key={index} className="post-item">{post}
+          <button onClick={() => deletePost(index)}> 삭제 </button>
+          </li>
         ))}
       </ul>
     </div>
